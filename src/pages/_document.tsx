b08e4f9ff3c0d36d3db02468a972e7ffee5a7852
João Cruz <joao.cruz@crmservices.com.br>
2021-10-30 02:17:18 -0300

    sitemap configurations

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -34,6 +34,11 @@ class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon.png"></link>
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+          <meta name="description" content="Geek News" />
+          <meta property="og:title" content="Geek News" />
+          <meta property="og:description" content="Notícias de Geek para Geek!" />
+          <meta property="og:url" content="https://portalgeeknews.com.br/" />
+          <meta property="og:type" content="website" />
           <meta name="google-site-verification" content="S_OKebA4xtKZP9Y6q8ug4G7JhxrrAQMecXCczfYtq6k" />
           <meta name="theme-color" content="#0f1519" />
           <meta charSet="utf-8" />
