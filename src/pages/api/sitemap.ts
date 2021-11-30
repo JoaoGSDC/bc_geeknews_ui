9a03c819923ddd710f08964eb1aff1af562374a8
João Cruz <joao.cruz@crmservices.com.br>
2021-11-30 16:23:55 -0300

    adjustment try catch in sitemap

--- src/pages/api/sitemap.ts
+++ src/pages/api/sitemap.ts
@@ -2,22 +2,26 @@ import { SitemapStream, streamToPromise } from 'sitemap';
 import { Readable } from 'stream';
 
 const Sitemap = async (req: any, res: any) => {
-  // An array with your links
-  const links = [
-    { url: '/categoria/[slug]', changefreq: 'daily', priority: 0.3 },
-    { url: '/noticia/[slug]', changefreq: 'daily', priority: 0.3 },
-  ];
+  try {
+    // An array with your links
+    const links = [
+      { url: '/categoria/[slug]', changefreq: 'daily', priority: 0.3 },
+      { url: '/noticia/[slug]', changefreq: 'daily', priority: 0.3 },
+    ];
 
-  // Create a stream to write to
-  const stream = new SitemapStream({ hostname: `https://${req.headers.host}` });
+    // Create a stream to write to
+    const stream = new SitemapStream({ hostname: `https://${req.headers.host}` });
 
-  res.writeHead(200, {
-    'Content-Type': 'application/xml',
-  });
+    res.writeHead(200, {
+      'Content-Type': 'application/xml',
+    });
 
-  const xmlString = await streamToPromise(Readable.from(links).pipe(stream)).then((data: any) => data.toString());
+    const xmlString = await streamToPromise(Readable.from(links).pipe(stream)).then((data: any) => data.toString());
 
-  res.end(xmlString);
+    res.end(xmlString);
+  } catch (error: any) {
+    return res.json(error);
+  }
 };
 
 export default Sitemap;
